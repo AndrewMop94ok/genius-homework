a131d963e0bd078be28dd56cf7180e6054e41564
AndriiHalchenko <153083144+AndrewMop94ok@users.noreply.github.com>
2024-03-26 12:00:04 +0200

    homework-6

---
+++ homework-js/JS/homework-6.js
@@ -0,0 +1,43 @@
+// Створити телефонну книгу
+// створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// створити set який прийматиме телефон і номер
+// створити get який виводитиме данні про абонента
+// створити три різних юзери
+// вивести данні
+
+// class Abonent {
+//   constructor(options) {
+//     this.name = options.name;
+//     this.number = options.number;
+//   }
+//   userName = "";
+//   userNumber = "";
+
+//   set name(newName) {
+//     const nameRow = newName.split(" ");
+//     const numberRow = newName.split(" ");
+//     this.userName = nameRow[0];
+//     this.userNumber = numberRow[1];
+//   }
+
+//   get name() {
+//     return `Abonent name - ${this.userName} tel. - ${this.userNumber}`;
+//   }
+// }
+
+// const firstAbon = new Abonent({
+//   name: "Andrew",
+//   number: "+380971234567",
+// });
+// const secondAbon = new Abonent({
+//   name: "Konstantin",
+//   number: "+380660001122",
+// });
+// const thirdAbon = new Abonent({
+//   name: "Yuriy",
+//   number: "+380735673421",
+// });
+
+// console.log(firstAbon);
+// console.log(secondAbon);
+// console.log(thirdAbon);
