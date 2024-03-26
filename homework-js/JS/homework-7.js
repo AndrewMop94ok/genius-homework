74b8acfa04f49198b38916bc1912f25f297b902a
AndriiHalchenko <153083144+AndrewMop94ok@users.noreply.github.com>
2024-03-26 15:02:51 +0200

    Homework-7

---
+++ homework-js/JS/homework-7.js
@@ -0,0 +1,24 @@
+// Створіть функцію-конструктор Calculator, який створює об’єкти з трьома
+// методами:
+// read() запитує два значення за допомогою prompt і запам’ятовує їх у
+// властивостях об’єкта.
+// sum() повертає суму цих властивостей.
+// mul() повертає результат множення даних властивостей
+
+// function Calculator() {
+//   this.read = function () {
+//     this.firstNumber = Number(prompt("Enter your first number"));
+//     this.secondNumber = Number(prompt("Enter your second number"));
+//   };
+//   this.sum = function () {
+//     return this.firstNumber + this.secondNumber;
+//   };
+//   this.mul = function () {
+//     return this.firstNumber * this.secondNumber;
+//   };
+// }
+
+// let calculator = new Calculator();
+// calculator.read();
+// console.log(`Your sum = `, calculator.sum());
+// console.log(`Your mul = `, calculator.mul());
